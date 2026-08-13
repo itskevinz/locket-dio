2a39dc1bbe2e0859aa7d7a489b739175c7aba899
buiduchuy2010qn-prog <buiduchuy2010qn@gmail.com>
2026-08-13 20:55:38 +0700

    fix(vercel): connect Vercel function to Express backend

--- api/vercel-web.js
+++ api/vercel-web.js
@@ -1,9 +1,5 @@
-module.exports = async function handler(req, res) {
-  res.statusCode = 503;
-  res.setHeader("content-type", "application/json; charset=utf-8");
-  res.end(JSON.stringify({
-    ok: false,
-    code: "VERCEL_DRIVE_MIGRATION_PENDING",
-    message: "Google Drive route is being migrated to Vercel"
-  }));
+const handler = require("./app.js");
+
+module.exports = function vercelWeb(req, res) {
+  return handler(req, res);
 };
