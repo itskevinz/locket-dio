83d2755034bb8d385596b973b9925dc1ec248d55
buiduchuy2010qn-prog <buiduchuy2010qn@gmail.com>
2026-08-16 23:13:45 +0700

    feat(friends): export friend tools service

--- src/services/LocketServices/index.js
+++ src/services/LocketServices/index.js
@@ -4,4 +4,5 @@ export * from "./chat.services";
 export * from "./rollcall.services";
 export * from "./fetch.services"
 export * from "./friend.services";
+export * from "./friendTools.services";
 export * from "./group.services";
